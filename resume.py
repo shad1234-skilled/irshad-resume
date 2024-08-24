561c2dc62afb3a90b4e2b61860f22f3c282ecefa
shad1234-skilled <shad.qaadri@gmail.com>
2024-08-24 11:35:54 +0530

    Update resume.py

--- resume.py
+++ resume.py
@@ -65,11 +65,13 @@ with st.container():
 st.markdown("<br>", unsafe_allow_html=True)
 #type_text(markdown_text)
 
-image_url = "https://github.com/shad1234-skilled/irshad-resume/blob/6d63d94ee24955eb6d171f820415a4488ad1d076/image.JPG"
+image_url = "https://github.com/shad1234-skilled/irshad
+-resume/blob/6d63d94ee24955eb6d171f820415a4488ad1d076/image.JPG"
 response = requests.get(image_url)
 image = Image.open(BytesIO(response.content))
 
-pdf_url = "https://github.com/shad1234-skilled/irshad-resume/blob/6d63d94ee24955eb6d171f820415a4488ad1d076/my_resume.pdf"
+pdf_url = "https://github.com/shad1234-skilled/irshad-
+resume/blob/6d63d94ee24955eb6d171f820415a4488ad1d076/my_resume.pdf"
 
  
 def sidebar():
