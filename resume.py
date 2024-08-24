410333b175359a4013c509b7e369bdb29cd3ca3d
shad1234-skilled <shad.qaadri@gmail.com>
2024-08-24 15:53:03 +0530

    Update resume.py

--- resume.py
+++ resume.py
@@ -164,7 +164,7 @@ def navigation_bar():
     
     st.markdown(f"""
     <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #008080;">
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="true" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
