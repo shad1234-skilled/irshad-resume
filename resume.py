b9988228a44d6759755e3fac14d20638c8f013cc
shad1234-skilled <shad.qaadri@gmail.com>
2024-08-24 15:05:48 +0530

    Update resume.py

--- resume.py
+++ resume.py
@@ -126,17 +126,17 @@ def sidebar():
     st.sidebar.write("📧 Email: mohammadirshad.khan@rediffmail.com")
     st.sidebar.write("🌐 [LinkedIn](https://www.linkedin.com/in/mohammad-irshad-khan-06242b255/) | [GitHub](https://github.com/shad1234-skilled)")
 
-    
+st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
+st.markdown('<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>', unsafe_allow_html=True)
+st.markdown('<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j5nQq4V6ZJ5v5TOF8h5T" crossorigin="anonymous"></script>', unsafe_allow_html=True)
+st.markdown('<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>', unsafe_allow_html=True)
+   
 def navigation_bar():
     #####################
     # Navigation
     
     st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
-    st.markdown("""
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-feJI7DjZZ02yU0d6NKJZ4BzYaRTLT4Rf8Cp5Qu8fTjlSyaOGpVz7w69d6ouojV6h" crossorigin="anonymous"></script>
-    """, unsafe_allow_html=True)
-    
+       
     st.markdown(f"""
     <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #008080;">
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
